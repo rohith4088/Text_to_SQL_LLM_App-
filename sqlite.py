bff115a5eed73818f8572c27a24f8d1484d6bc9d
rohith4088 <2109486@reva.edu.in>
2024-01-27 09:56:44 +0530

    updated db

--- sqlite.py
+++ sqlite.py
@@ -17,11 +17,11 @@ cursor.execute(table_info)
 
 ## Insert Some more records
 
-cursor.execute('''Insert Into STUDENT values('Krish','Data Science','A',90)''')
-cursor.execute('''Insert Into STUDENT values('Sudhanshu','Data Science','B',100)''')
-cursor.execute('''Insert Into STUDENT values('Darius','Data Science','A',86)''')
-cursor.execute('''Insert Into STUDENT values('Vikash','DEVOPS','A',50)''')
-cursor.execute('''Insert Into STUDENT values('Dipesh','DEVOPS','A',35)''')
+cursor.execute('''Insert Into STUDENT values('rohith','Data Science','A',90)''')
+cursor.execute('''Insert Into STUDENT values('pooja','Data Science','B',100)''')
+cursor.execute('''Insert Into STUDENT values('milo','Data Science','A',86)''')
+cursor.execute('''Insert Into STUDENT values('max','AI','A',50)''')
+cursor.execute('''Insert Into STUDENT values('nobody','AI','A',35)''')
 
 ## Disspaly ALl the records
 
